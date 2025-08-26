9bae63beb752a3a0adbefd21f589a29fe4d08c52
Will Jones <wtjones10@gmail.com>
2025-08-26 12:56:42 +0100

    UPDATE | Fixed pipeline button

--- components/SecondaryCta.jsx
+++ components/SecondaryCta.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const SecondaryCta = () => {
   return (
-    <Link href="/#keywords" className="">
+    <Link href="/#pipeline" className="">
         <button className="hover:cursor-pointer hover:opacity-90 transition-all duration-200 bg-white border border-gray-300 text-gray-900 px-4 py-2 rounded-lg text-sm tracking-tight">
             Pipeline Impact
         </button>
